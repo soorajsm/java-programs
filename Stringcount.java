28b4d179e8f541b3a75d1ec8c93f54407753c43e
Sooraj <soorajsm56@gmail.com>
2023-08-04 19:38:46 +0530

    04aug23

--- Stringcount.java
+++ Stringcount.java
@@ -1,30 +1,34 @@
 import java.util.Arrays;
-
 import java.util.Scanner;
-
 public class Stringcount {
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i=0,count=0;
+        boolean repeated=false;
+        int count=0;
         String a[]=new String[n];
         String str[]=new String[n];
-        while(i<n)
+        for( int i=0;i<n;i++)
         {
             a[i]=sc.next();
         }
+
         int loc=sc.nextInt();
-        Arrays.sort(a);
-        for(int j=0;j<n;j++)
+        for( int i=0;i<n;i++)
         {
-            if(a[j]!=a[j+1])
-            {
-                str[count++]=a[j];
-            }
+            repeated=false;
+           for(int j=0;j<n;j++)
+           {
+                if(i!=j && a[i].equals(a[j]))
+                {
+                    repeated=true;
+                    break;
+                }
+           }
+           if(repeated==false)
+           str[count++]=a[i];
         }
-        System.out.println(str[loc]);
+        System.out.println(str[loc-1]);
     }
-    
 }
  
